23e15b6962b4b7b5059527292acc5e2ffd058f50
sherm8n <sherm8n@gmail.comm>
2011-12-02 23:58:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifesense::Application.config.secret_token = '5ff6d2f4c8b0c8972ffaeb6c37b177f608ba0401ba6b37078b1c0134b582420f3b4f286326df5774ab835c9ebca9b2e7d8318e81d5295f052d8b1e6aae2c0de2'
